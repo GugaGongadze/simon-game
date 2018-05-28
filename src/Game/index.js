1550b03ec4358a096647a6b88a60ab20a3a21c09
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-05-28 16:05:56 +0400

    Added sounds playing functionality

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './index.css';
+import redSound from '../Sounds/simonSound1.mp3';
+import blueSound from '../Sounds/simonSound2.mp3';
+import yellowSound from '../Sounds/simonSound3.mp3';
+import greenSound from '../Sounds/simonSound4.mp3';
 
 class Game extends React.Component {
   constructor(props) {
@@ -9,21 +13,55 @@ class Game extends React.Component {
       gameOn: false,
       strictModeEnabled: false,
       gameStarted: false,
-      count: null
-    }
+      count: null,
+      level: 0,
+      currentSteps: []
+    };
 
-    this.onSwitchChange = this.onSwitchChange.bind(this)
+    this.redButton = React.createRef();
+    this.blueButton = React.createRef();
+    this.yellowButton = React.createRef();
+    this.greenButton = React.createRef();
+
+    this.onSwitchChange = this.onSwitchChange.bind(this);
+    this.onGameStart = this.onGameStart.bind(this);
   }
 
-  onSwitchChange = newSwitch => {
+  onSwitchChange = () => {
     if (this.state.gameOn === false) {
       this.setState({
         gameOn: true
-      })
+      });
     } else {
       this.setState({
-        gameOn: false
-      })
+        gameOn: false,
+        strictModeEnabled: false,
+        gameStarted: false,
+        count: null
+      });
+    }
+  };
+
+  onGameStart = () => {
+    if (this.state.gameOn) {
+      this.setState({
+        gameStarted: true,
+        count: 0
+      });
+    }
+
+    this.playSound(chooseRandomColor());
+  };
+
+  playSound = (color) => {
+    if (color === 'red') {
+      this.redButton.current.play()
+    } else if (color === 'blue') {
+      this.blueButton.current.play()
+    } else if (color === 'yellow') {
+      this.yellowButton.current.play()
+    } else {
+      this.greenButton.current.play()
     }
   }
 
@@ -31,13 +69,20 @@ class Game extends React.Component {
     return (
       <div className="game-board">
         <div className="top-buttons">
-          <div className="button green-button" />
-          <div className="button red-button" />
+          <div className="button green-button">
+            <audio ref={this.greenButton} src={greenSound} />
+          </div>
+          <div className="button red-button">
+            <audio ref={this.redButton} src={redSound} />
+          </div>
         </div>
-
         <div className="bottom-buttons">
-          <div className="button yellow-button" />
-          <div className="button blue-button" />
+          <div className="button yellow-button">
+            <audio ref={this.yellowButton} src={yellowSound} />
+          </div>
+          <div className="button blue-button">
+            <audio ref={this.blueButton} src={blueSound} />
+          </div>
         </div>
 
         <div className="dashboard">
@@ -47,21 +92,24 @@ class Game extends React.Component {
               <div className="display">
                 <div className="screen-frame">
                   <div className="screen">
-                    <span className="count-output">{this.state.gameOn === true ? '--' : this.state.gameOn}</span>
+                    <span className="count-output">
+                      {this.state.gameOn === true
+                        ? this.state.gameStarted
+                          ? this.state.count
+                          : '--'
+                        : this.state.gameOn}
+                    </span>
                   </div>
                 </div>
                 <p className="display-text text-uppercase mt-5">Count</p>
               </div>
-              <div className="start-button">
-                <div className="start-button-outline">
-                </div>
+              <div className="start-button" onClick={this.onGameStart}>
+                <div className="start-button-outline" />
                 <p className="display-text text-uppercase mt-5">Start</p>
               </div>
               <div className="strict-mode-button">
-                <div className="strict-mode-indicator">
-                </div>
-                <div className="strict-mode-button-outline">
-                </div>
+                <div className="strict-mode-indicator" />
+                <div className="strict-mode-button-outline" />
                 <p className="display-text text-uppercase mt-5">Strict</p>
               </div>
             </div>
@@ -79,6 +127,19 @@ class Game extends React.Component {
         </div>
       </div>
     );
+  }
+}
+
+function chooseRandomColor() {
+  const randomNumber = Math.round(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    return 'red';
+  } else if (randomNumber === 2) {
+    return 'blue';
+  } else if (randomNumber === 3) {
+    return 'yellow';
+  } else {
+    return 'green';
   }
 }
 
