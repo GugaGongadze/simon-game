284265f443df5c953a6ea97afecc8861b1df5c39
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-05-28 17:00:42 +0400

    Added animations when sounds are played

--- src/Game/index.js
+++ src/Game/index.js
@@ -22,9 +22,14 @@ class Game extends React.Component {
     this.blueButton = React.createRef();
     this.yellowButton = React.createRef();
     this.greenButton = React.createRef();
+    this.redButtonWrapper = React.createRef();
+    this.blueButtonWrapper = React.createRef();
+    this.yellowButtonWrapper = React.createRef();
+    this.greenButtonWrapper = React.createRef();
 
     this.onSwitchChange = this.onSwitchChange.bind(this);
     this.onGameStart = this.onGameStart.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this); // Redundant but I like to keep all event handlers explicitly declared
   }
 
   onSwitchChange = () => {
@@ -37,7 +42,8 @@ class Game extends React.Component {
         gameOn: false,
         strictModeEnabled: false,
         gameStarted: false,
-        count: null
+        count: null,
+        currentSteps: []
       });
     }
   };
@@ -48,51 +54,109 @@ class Game extends React.Component {
         this.setState({
           count: 0,
           currentSteps: []
-        })
+        });
       } else {
         this.setState({
           gameStarted: true,
           count: 0
         });
-        this.playSound(chooseRandomColor());
       }
+      this.playSound(chooseRandomColor());
     }
-
   };
 
-  playSound = (color) => {
+  playSound = color => {
     if (color === 'red') {
-      this.redButton.current.play()
+      this.redButtonWrapper.current.style.opacity = "0.5";
+      this.redButton.current.play();
+        setTimeout(() => {
+          this.redButtonWrapper.current.style.opacity = "1";
+        }, this.redButton.current.duration * 1000);
     } else if (color === 'blue') {
-      this.blueButton.current.play()
+      this.blueButtonWrapper.current.style.opacity = "0.5";
+      this.blueButton.current.play();
+        setTimeout(() => {
+          this.blueButtonWrapper.current.style.opacity = "1";
+        }, this.blueButton.current.duration * 1000);
+      this.blueButton.current.play();
     } else if (color === 'yellow') {
-      this.yellowButton.current.play()
+      this.yellowButtonWrapper.current.style.opacity = "0.5";
+      this.yellowButton.current.play();
+        setTimeout(() => {
+          this.yellowButtonWrapper.current.style.opacity = "1";
+        }, this.yellowButton.current.duration * 1000);
+      this.yellowButton.current.play();
     } else if (color === 'green') {
-      this.greenButton.current.play()
+      this.greenButtonWrapper.current.style.opacity = "0.5";
+      this.greenButton.current.play();
+        setTimeout(() => {
+          this.greenButtonWrapper.current.style.opacity = "1";
+        }, this.greenButton.current.duration * 1000);
+      this.greenButton.current.play();
     }
 
     this.setState(prevState => ({
       count: prevState.count + 1,
       currentSteps: [...prevState.currentSteps, color]
-    }))
+    }));
+  };
+
+  continueGame = () => {
+    this.playSound(chooseRandomColor())
   }
+
+  onButtonClick = userGuess => {
+    const currentMove = this.state.currentSteps[
+      this.state.currentSteps.length - 1
+    ];
+    if (userGuess !== currentMove) {
+      this.setState({
+        count: null,
+        level: 0,
+        currentSteps: []
+      });
+    } else {
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+        level: prevState.level + 1,
+      }));
+
+      this.continueGame();
+    }
+  };
 
   render() {
     return (
       <div className="game-board">
         <div className="top-buttons">
-          <div className="button green-button">
+          <div
+            className="button green-button"
+            onClick={() => this.onButtonClick('green')}
+            ref={this.greenButtonWrapper}
+          >
             <audio ref={this.greenButton} src={greenSound} />
           </div>
-          <div className="button red-button">
+          <div
+            className="button red-button"
+            onClick={() => this.onButtonClick('red')}
+            ref={this.redButtonWrapper}
+          >
             <audio ref={this.redButton} src={redSound} />
           </div>
         </div>
         <div className="bottom-buttons">
-          <div className="button yellow-button">
+          <div
+            className="button yellow-button"
+            onClick={() => this.onButtonClick('yellow')}
+            ref={this.yellowButtonWrapper}
+          >
             <audio ref={this.yellowButton} src={yellowSound} />
           </div>
-          <div className="button blue-button">
+          <div
+            className="button blue-button"
+            onClick={() => this.onButtonClick('blue')}
+            ref={this.blueButtonWrapper}
+          >
             <audio ref={this.blueButton} src={blueSound} />
           </div>
         </div>
